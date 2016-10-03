25fc8a289a98204f324bdb33351bfa07f2741203
Samantha Monteiro <samanthakem@gmail.com>
2016-10-03 10:54:21 -0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+module.exports = {
+  name: {
+    first: 'Samantha',
+    last: 'Monteiro',
+  },
+  twitter: 'https://twitter.com/samanthakem_',
+  github: 'https://github.com/samanthakem',
+  email: 'samanthakem@gmail.com'
+}
